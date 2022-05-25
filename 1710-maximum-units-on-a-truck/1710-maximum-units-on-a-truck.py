866f32c1c0e63ba26a633ee2259c0ca090f8ddb1
Kirill <36884521+kirill-menke@users.noreply.github.com>
2022-05-25 18:29:43 +0200

    Time: 181 ms (67.06%), Space: 14.5 MB (32.33%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,20 +1,24 @@
+from collections import defaultdict
+
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
         
-        # Runtime:  O(n * log(k)) since our heap has size k and we need to pop n times in worst case
-        # Space:    O(k) since out heap has size k
+        # Runtime:  O(n) + O(1000) = O(n) where n is the length of boxTypes array
+        # Space:    O(1000) = O(1)
         
-        n = len(boxTypes)
-        heap = []
+        buckets = defaultdict(int)
+        total_weight = 0
         
-        for i in range(n):
-            for j in range(boxTypes[i][0]):
-                weight = boxTypes[i][1]
-                if len(heap) < truckSize:
-                    heapq.heappush(heap, weight)
-                elif heap[0] < weight:
-                    heapq.heappushpop(heap, weight)
+        for num, weight in boxTypes:
+            buckets[weight] += num
         
-        return sum(heap)
-        
+        for w in range(1000, 0, -1):
+            num = min(truckSize, buckets[w])
+            total_weight += num * w
+            truckSize -= num
+            if truckSize == 0:
+                return total_weight
+            
+        return total_weight
+            
         
