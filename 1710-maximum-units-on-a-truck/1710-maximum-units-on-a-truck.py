f204db3704eb9b0f9302330c69cf7b2f2bb76930
Kirill <36884521+kirill-menke@users.noreply.github.com>
2022-05-28 19:08:34 +0200

    Time: 5683 ms (5.02%), Space: 18.3 MB (32.20%) - LeetHub

--- 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
+++ 1710-maximum-units-on-a-truck/1710-maximum-units-on-a-truck.py
@@ -1,24 +1,11 @@
-from collections import defaultdict
-
 class Solution:
     def maximumUnits(self, boxTypes: List[List[int]], truckSize: int) -> int:
+        heap = []
         
-        # Runtime:  O(n) + O(1000) = O(n) where n is the length of boxTypes array
-        # Space:    O(1000) = O(1)
+        for box in boxTypes:
+            for i in range(box[0]):
+                heapq.heappush(heap, box[1])
+                if len(heap) > truckSize:
+                    heapq.heappop(heap)
         
-        buckets = defaultdict(int)
-        total_weight = 0
-        
-        for num, weight in boxTypes:
-            buckets[weight] += num
-        
-        for w in range(1000, 0, -1):
-            num = min(truckSize, buckets[w])
-            total_weight += num * w
-            truckSize -= num
-            if truckSize == 0:
-                return total_weight
-            
-        return total_weight
-            
-        
+        return sum(heap)
