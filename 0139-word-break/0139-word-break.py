4d9c039ef766aa7776c0f380453bf13741a2d403
Kirill <36884521+kirill-menke@users.noreply.github.com>
2023-10-03 15:34:02 +0200

    Time: 48 ms (34.25%), Space: 16.3 MB (94.18%) - LeetHub

--- 0139-word-break/0139-word-break.py
+++ 0139-word-break/0139-word-break.py
@@ -1,15 +1,12 @@
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
-        @cache
-        def check(start):
-            if start >= len(s):
-                return True
-            
-            for i in range(start + 1, len(s) + 1):
-                if s[start: i] in wordDict and check(i):
-                    return True
-            
-            return False
+        table = [False] * (len(s) + 1)
+        table[0] = True
 
-        return check(0)
+        for i, char in enumerate(s, 1):
+            for prev in range(i - 1, -1, -1):
+                if s[prev: i] in wordDict and table[prev]:
+                    table[i] = True
+                    break
+
+        return table[-1]
