121eab40e284ba65688ea7200b07045ea0a739f6
Kirill <36884521+kirill-menke@users.noreply.github.com>
2023-10-11 16:46:24 +0200

    Time: 110 ms (18.36%), Space: 15.9 MB (99.89%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -1,18 +1,23 @@
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        row_sets = [set() for _ in range(len(board))]
-        col_sets = [set() for _ in range(len(board[0]))]
-        box_sets = [set() for _ in range(len(board))]
+        n = len(board)
+        row_content = [0 for _ in range(n)]
+        col_content = [0 for _ in range(n)]
+        box_content = [0 for _ in range(n)]
 
-        for i, row in enumerate(board):
-            for j, num in enumerate(row):
-                if num == '.':
+        for i in range(n):
+            for j in range(n):
+                if board[i][j] == '.':
                     continue
-                if num in row_sets[i] or num in col_sets[j] or num in box_sets[i // 3 * 3 + j // 3]:
+
+                num = int(board[i][j]) - 1
+                box_cell = i // 3 * 3 + j // 3
+                if row_content[i] & (1 << num) or col_content[j] & (1 << num) or box_content[box_cell] & (1 << num):
                     return False
                 
-                row_sets[i].add(num)
-                col_sets[j].add(num)
-                box_sets[i // 3 * 3 + j // 3].add(num)
+                row_content[i] |= (1 << num)
+                col_content[j] |= (1 << num)
+                box_content[box_cell] |= (1 << num)
         
         return True
+                
