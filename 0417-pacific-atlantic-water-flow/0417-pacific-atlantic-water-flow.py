5987e367aee20c35d75fced0004a32ee27e36aba
Kirill <36884521+kirill-menke@users.noreply.github.com>
2023-10-13 12:58:35 +0200

    Time: 242 ms (62.72%), Space: 18 MB (44.47%) - LeetHub

--- 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
+++ 0417-pacific-atlantic-water-flow/0417-pacific-atlantic-water-flow.py
@@ -1,45 +1,34 @@
-from collections import deque
-
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        pacific_cells = set()
-        atlantic_cells = set()
+        height, width = len(heights), len(heights[0])
+        pacific_indices = list(zip([0] * width + [*range(1, height)], [*range(width)] + [0] * (height - 1)))
+        pacific_visited = set([*pacific_indices])
 
-        height = len(heights)
-        width = len(heights[0])
-
-        pacific_indices = list(zip([0] * width + [*range(height)], [*range(width)] + [0] * height))
-        queue = deque([*pacific_indices])
-        pacific_cells |= {*pacific_indices}
-        
+        queue = collections.deque([*pacific_indices])
         while queue:
             i, j = queue.pop()
-            
             for y, x in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
                 row, col = i + y, j + x
-        
-                if 0 <= row < height and 0 <= col < width and (row, col) not in pacific_cells and heights[row][col] >= heights[i][j]:
-                    pacific_cells.add((row, col))
+                if 0 <= row < height and 0 <= col < width \
+                    and heights[row][col] >= heights[i][j] and (row, col) not in pacific_visited:
                     queue.appendleft((row, col))
-
-        atlantic_indices = list(zip([*range(height)] + [height - 1] * width, [width - 1] * (height - 1) + [*range(width)]))
+                    pacific_visited.add((row, col))
         
-        queue = deque([*atlantic_indices])
-        atlantic_cells |= {*atlantic_indices}
+
+        atlantic_indices = list(zip([*range(height - 1)] + [height - 1] * width, [width - 1] * (height - 1) + [*range(width)]))
+        atlantic_visited = set([*atlantic_indices])
+        queue = collections.deque([*atlantic_indices])
 
         while queue:
             i, j = queue.pop()
-            
             for y, x in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
                 row, col = i + y, j + x
-                if 0 <= row < height and 0 <= col < width and (row, col) not in atlantic_cells and heights[row][col] >= heights[i][j]:
-                    atlantic_cells.add((row, col))
+                if 0 <= row < height and 0 <= col < width \
+                    and heights[row][col] >= heights[i][j] and (row, col) not in atlantic_visited:
                     queue.appendleft((row, col))
+                    atlantic_visited.add((row, col))
         
-
-        return pacific_cells & atlantic_cells
-                    
-
+        return pacific_visited & atlantic_visited
 
 
         
