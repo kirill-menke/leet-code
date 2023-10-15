d2282b4fba880bb74f5cf994e2759de705b1f941
Kirill <36884521+kirill-menke@users.noreply.github.com>
2023-10-15 14:32:14 +0200

    Time: 68 ms (31.29%), Space: 16.3 MB (91.06%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,28 +1,23 @@
 from collections import Counter
-from heapq import heappop, heappush, heapreplace
+from heapq import heappush, heappop
 
 class Pair:
     def __init__(self, count, word):
         self.count = count
         self.word = word
-    
-    def __gt__(self, pair):
-        return self.count > pair.count or (self.count == pair.count and self.word < pair.word)
+
+    def __lt__(self, pair):
+        return self.count < pair.count or (self.count == pair.count and self.word > pair.word)
 
 
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
-        word_count = list(Counter(words).items())
-        heap = [Pair(count, s) for s, count in word_count[:k]]
-        heapify(heap)
-        
-        for i in range(k, len(word_count)):
-            if heap[0] < Pair(word_count[i][1], word_count[i][0]):
-                heapreplace(heap, Pair(word_count[i][1], word_count[i][0]))
-        
+        word_count = Counter(words)
+        heap = []
 
+        for word, count in word_count.items():
+            heappush(heap, Pair(count, word))
+            if len(heap) > k:
+                heappop(heap)
+        
         return reversed([heappop(heap).word for _ in range(k)])
-
-
-            
-        
