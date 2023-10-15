6ff3d8f41bc241c47bf3bb2b642af974de096431
Kirill <36884521+kirill-menke@users.noreply.github.com>
2023-10-15 15:05:45 +0200

    Time: 1240 ms (94.31%), Space: 58.1 MB (87.72%) - LeetHub

--- 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
+++ 0211-design-add-and-search-words-data-structure/0211-design-add-and-search-words-data-structure.py
@@ -1,38 +1,37 @@
-class TrieNode:
-    def __init__(self):
-        self.links = [None] * 26
-        self.is_end = False
-
 class WordDictionary:
 
     def __init__(self):
-        self.root = TrieNode()
+        self.root = {}
 
     def addWord(self, word: str) -> None:
-        curr = self.root
+        current = self.root
+
         for char in word:
-            if not curr.links[ord(char) - ord('a')]:
-                curr.links[ord(char) - ord('a')] = TrieNode()
-            curr = curr.links[ord(char) - ord('a')]    
-    
-        curr.is_end = True
+            if char not in current:
+                current[char] = {}
+            current = current[char]
+        
+        current['$'] = True
+
 
     def search(self, word: str) -> bool:
-        def search_helper(word, start_node):
-            curr = start_node
+        
+        def search_helper(start_node, word):
             for i, char in enumerate(word):
                 if char == '.':
-                    for node in curr.links:
-                        if node and search_helper(word[i + 1:], node):
+                    for link in start_node:
+                        if link != '$' and search_helper(start_node[link], word[i + 1:]):
                             return True
                     return False
-                if not curr.links[ord(char) - ord('a')]:
+                elif char not in start_node:
                     return False
-                curr = curr.links[ord(char) - ord('a')]
+                else:
+                    start_node = start_node[char]
             
-            return curr.is_end
+            return '$' in start_node
 
-        return search_helper(word, self.root)
+        return search_helper(self.root, word)
+
 
 
 # Your WordDictionary object will be instantiated and called as such:
