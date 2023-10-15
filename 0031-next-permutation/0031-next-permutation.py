cba9af8a747cbb7ad91151cdcb32d5b8b8d27935
Kirill <36884521+kirill-menke@users.noreply.github.com>
2023-10-15 13:34:09 +0200

    Time: 46 ms (69.32%), Space: 16.4 MB (22.28%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.py
+++ 0031-next-permutation/0031-next-permutation.py
@@ -1,29 +1,26 @@
 class Solution:
+    def reverse(self, nums, start):
+        for i, elem in enumerate(reversed(nums[start:]), start):
+            nums[i] = elem
+
     def nextPermutation(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
         """
-        start_idx = None
+        swap_idx = -1
 
         for i in range(len(nums) - 1, 0, -1):
             if nums[i] > nums[i - 1]:
-                start_idx = i - 1
+                swap_idx = i - 1
                 break
         
-        if start_idx is None:
-            nums.sort()
+        if swap_idx == -1:
+            self.reverse(nums, 0)
             return
 
-        for j in range(len(nums) - 1, start_idx, -1):
-            if nums[j] > nums[start_idx]:
-                nums[j], nums[start_idx] = nums[start_idx], nums[j]
+        for i in range(len(nums) - 1, 0, -1):
+            if nums[i] > nums[swap_idx]:
+                nums[i], nums[swap_idx] = nums[swap_idx], nums[i]
                 break
-        
-        for i, num in enumerate(reversed(nums[start_idx + 1:]), start_idx + 1):
-            nums[i] = num
-        
 
-            
-
-
-        
+        self.reverse(nums, swap_idx + 1)
