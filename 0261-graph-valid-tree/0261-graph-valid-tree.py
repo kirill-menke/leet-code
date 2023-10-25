e3f0684245d7133f6011283f897cf6ed60396dde
Kirill <36884521+kirill-menke@users.noreply.github.com>
2023-10-25 12:08:24 +0200

    Time: 103 ms (17.99%), Space: 17.6 MB (94.64%) - LeetHub

--- 0261-graph-valid-tree/0261-graph-valid-tree.py
+++ 0261-graph-valid-tree/0261-graph-valid-tree.py
@@ -1,36 +1,21 @@
-from collections import deque
-
 class Solution:
     def validTree(self, n: int, edges: List[List[int]]) -> bool:
-        if n == 1:
-            return True
-        if len(edges) < n - 1:
-            return False
-
-        graph = {i: [] for i in range(n)}
-        degree = [0] * n
-
+        graph = [[] for _ in range(n)]
         for n1, n2 in edges:
             graph[n1].append(n2)
             graph[n2].append(n1)
-            degree[n1] += 1
-            degree[n2] += 1
-        
-        queue = deque([])
-        visited = set()
 
-        for i, d in enumerate(degree): 
-            if d == 1:
-                queue.append(i)
-                visited.add(i)
+        stack = [(0, -1)]
+        visited = set([0])
 
-        while queue:
-            node = queue.pop()
+        while stack:
+            node, parent = stack.pop()
+            for neigh in graph[node]:
+                if neigh == parent:
+                    continue
+                if neigh in visited:
+                    return False
+                stack.append((neigh, node))
+                visited.add(neigh)
             
-            for neighbor in graph[node]:
-                degree[neighbor] -= 1
-                if neighbor not in visited and degree[neighbor] == 1:
-                    queue.appendleft(neighbor)
-                    visited.add(neighbor)
-        
         return len(visited) == n
