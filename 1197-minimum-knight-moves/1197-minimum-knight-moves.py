e7f41984bae134136748c082dabe87e2ccc7bb42
Kirill <36884521+kirill-menke@users.noreply.github.com>
2023-11-27 13:31:37 +0100

    Time: 68 ms (77.58%), Space: 18 MB (81.01%) - LeetHub

--- 1197-minimum-knight-moves/1197-minimum-knight-moves.py
+++ 1197-minimum-knight-moves/1197-minimum-knight-moves.py
@@ -1,16 +1,16 @@
-from collections import deque
-
 class Solution:
     def minKnightMoves(self, x: int, y: int) -> int:
         
         @cache
         def dfs(x, y):
-            if x == 0 and y == 0:
+            if x + y == 0:
                 return 0
-            if x + y == 2:
+            elif x + y == 2:
                 return 2
             
-            return min(dfs(abs(x - 2), abs(y - 1)), dfs(abs(x - 1), abs(y - 2))) + 1
-        
+            return min(
+                dfs(abs(x - 2), abs(y - 1)), 
+                dfs(abs(x - 1), abs(y - 2))
+            ) + 1
+                       
         return dfs(abs(x), abs(y))
-        
