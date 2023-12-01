1cd3c6a380a9d943692ec6bb99d8e6d6b2c59361
Kirill <36884521+kirill-menke@users.noreply.github.com>
2023-12-01 11:37:25 +0100

    Time: 45 ms (32.81%), Space: 16.6 MB (69.64%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+        
+        def helper(current, opened=0, closed=0):
+            if len(current) == 2 * n:
+                result.append(''.join(current))
+            else:
+                if opened < n:
+                    current.append('(')
+                    helper(current, opened + 1, closed)
+                    current.pop()
+                
+                if opened > 0 and closed < opened:
+                    current.append(')')
+                    helper(current, opened, closed + 1)
+                    current.pop()
+                    
+        helper([])
+        return result
